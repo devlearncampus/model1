d3e4bb1ae082a9b28d040500065a23f165f176d3
devlearncampus <devlearncampus.com>
2025-12-01 17:49:00 +0900

    init

---
+++ src/main/java/com/ch/model1/repository/BoardDAO.java
@@ -0,0 +1,170 @@
+package com.ch.model1.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.ch.model1.dto.Board;
+import com.ch.model1.util.PoolManager;
+
+//데이터베이스의 Board table 에 대한 CURD를 수행하는 객체 
+public class BoardDAO {
+	
+	PoolManager pool=new PoolManager();
+	
+	//Create(=insert)
+	//글 1건을 등록하는 메서드 
+	public int insert(Board board) {//개발 시 파라미터의 수가 많을때는 낱개로 처리하지 않음,
+														//특히 데이터베이스 연동 로직에서는 DTO 를 이용..
+		//이 메서드 호출 시 마다, 접속을 일으키는 것이 아니라, Tomcat 이 접속자가 없더라도
+		//미리 Connection 들을 확보해 놓은 커넥션풀(Connection Pool)로부터 대여해보자 
+		//또한 쿼리문 수행이 완료되더라도, 얻어온 Connection 절대로 닫지 말아야 한다, 반납해야 한다
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int result=0;  // return 할 예정이므로 try문 밖에 선언!!
+		
+		try {
+			InitialContext context = new InitialContext();
+			DataSource pool=(DataSource)context.lookup("java:comp/env/jndi/mysql");
+			con=pool.getConnection();
+			
+			//쿼리수행
+			String sql="insert into board(title, writer, content) values(?,?,?)";
+			pstmt=con.prepareStatement(sql);
+			
+			pstmt.setString(1, board.getTitle());
+			pstmt.setString(2, board.getWriter());
+			pstmt.setString(3, board.getContent());
+			
+			result = pstmt.executeUpdate();//쿼리 수행 
+			
+			/* 아래의 코드를 작성하면 않되는 이유?  out을 쓰려고 하는 순간 부터 BoardDAO의 중립성이 사라짐..
+			 * 해결책, DAO는 디자인 영역과는 분리된 오직 DB만을 전담하므로, 절대로 디자인코드를 넣어서는 안됨!!
+			 *         따라서 디자인 처리는 이 메서드를 호출한자가 처리하도록 , 여기서는 결과만 반환하면 됨...
+			if(result <1) {
+				System..print("실패");
+			}else {
+				out.print("성공");
+			}
+			*/
+			
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}	
+			if(con !=null) {
+				try {
+					//주의 기존 JDBC코드는 다 사용한 커넥션을 닫았지만, 풀로부터 얻어온 커넥션은 닫으면 안됨...
+					//이 객체는 DataSource 구현체로부터 얻어온 Connection 이기 때문에 일반적 JDBC 의 닫는 close()가 아님
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			
+		}
+		
+		return result;
+		
+	}
+	
+	
+	//Read(=select) - 모든 데이터 가져오기 
+	public List selectAll() {
+		//커넥션 얻는 코드를 이 메서드에서 손수하지말자!! PoolManager 가 대신 해주므로...
+		Connection con=pool.getConnection();//풀매니저로부터 커넥션 객체를 얻어옴!! ,여기서 직접 검색하면 jndi 검색코드 중복되므로..
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<Board> list=new ArrayList();//모든 게시물을 모아놓을 리스트...여기에 들어가는 객체는 바로 DTO 인스턴스 들이다..
+														//현재까지는 아무것도 채워넣지 않았기 때문에 size 가 0 이다!
+		
+		try {
+			String sql="select board_id, title, writer, regdate, hit from  board ";
+			pstmt=con.prepareStatement(sql);
+			rs=pstmt.executeQuery();//select문 의 반환값은 ResultSet 임 
+			
+			//rs는 무조건 이 메서드에서 닫아야 하므로, 외부의 jsp는 디자인을 담당하는 코드이지, ResultSet의 존재를 알필요도 없고,
+			//또한 ResulSet db연동기술이므로, 오직 DAO에서만 제어해야 한다..따라서 finally에서 rs를 닫는것은 DAO의 의무이다!!
+			//모순 - rs를 닫아버린 상태에서 외부 객체에게 전달해주면 외부객체는 이 rs를 사용할 수 없다 closed되어 있으므로,.
+			//해결책?? rs가 죽어도 상관없는 비슷한 유형의 객체로 데이터를 표현하면 된다...
+			//	         이 문제를 해결하기 위해 필요한 객체들의 조건 
+			//			1) 현실에 존재하는 사물을 표현할 수 잇는 객체가 필요하다..(예- 게시물 1건을 담을수 있는 존재) -Board  DTO
+			//			2) Board DTO로부터 생성된 게시물을 표현한 인스턴스들을 모아놓을 객체가 필요하다(순서O, 객체를 담을수 있어야함)
+			//              이 조건을 만족하는 객체는? 자바의 컬렉션 프레임웍 중 List 이다!!
+			// collection framework 이란? java.util 에서 지원하는 라이브러리로서, 오직 객체만을 모아서 처리할때 유용한 api	
+			
+			while(rs.next()) { //커서를 이동하면서 true 인 동안만...즉 모든 레코드 만큼...
+				Board board = new Board();  //게시물 한건을 담을 수 잇는 Board DTO 클래스의 인스턴스 1개 준비..(텅..) 
+				board.setBoard_id(rs.getInt("board_id")); //pk 담기 
+				board.setTitle(rs.getString("title")); //title 담기 
+				board.setWriter(rs.getString("writer"));//작성자 담기 
+				board.setRegdate(rs.getString("regdate"));
+				board.setHit(rs.getInt("hit"));
+				
+				list.add(board); //List에 인스턴스 1개 추가
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}	
+			
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}	
+			if(con !=null) {
+				try {
+					//주의 기존 JDBC코드는 다 사용한 커넥션을 닫았지만, 풀로부터 얻어온 커넥션은 닫으면 안됨...
+					//이 객체는 DataSource 구현체로부터 얻어온 Connection 이기 때문에 일반적 JDBC 의 닫는 close()가 아님
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				} 
+			}
+			
+		}
+		return list; //rs를 대용할 수 잇는 더욱 객체지향적인 형태로 반환...
+	}
+	
+	
+	//U(=update)
+	public void update() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");//드라이버 로드 
+			DriverManager.getConnection("jdbc:mysql://localhost", "servlet", "1234");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		
+	}
+	
+	//D(=delete)
+	
+}
---
+++ src/main/webapp/board/list.jsp
@@ -0,0 +1,75 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ page import="com.ch.model1.repository.BoardDAO" %>
+<%@ page import="java.sql.ResultSet" %>
+<%@ page import="java.util.List" %>
+<%@ page import="com.ch.model1.dto.Board" %>
+<%!
+	//!를 표기하면, 선언부를 의미...선언부란 이 jsp가 추후 서블릿으로 변경될때 멤버영역
+	BoardDAO boardDAO=new BoardDAO();
+%>
+<%
+	//이 영역은 스크립틀릿이기 때문에, 이 jsp파일이 서블릿으로 변환될때 service() 영역이므로
+	//얼마든지 DB연동 가능하다..
+	//하지만 하면 안됨!!!!!  그 이유?? 디자인 코드와 데이터베이스 연동코드가 하나로 합쳐(스파게티)져 있으면 
+	//추후 DB연동 코드를 재사용할 수 없다...
+	List<Board> list=boardDAO.selectAll(); //모든 레코드 가져오기!!!
+	out.print("등록된 게시물 수는 "+ list.size());
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=UTF-8>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+	width: 100%;
+	border: 1px solid #ddd;
+}
+
+th, td {
+	text-align: left;
+	padding: 16px;
+}
+
+tr:nth-child(even) {
+	background-color: #f2f2f2;
+}
+</style>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<th>No</th>
+			<th>제목</th>
+			<th>작성자</th>
+			<th>등록일</th>
+			<th>조회수</th>
+		</tr>
+		
+		<%
+			//rs에 들어있는 레코드들을 한칸씩 이동하면서 꺼내자 출력하자
+			//rs.next()가 true인 동아(즉 레코드가 존재하는 만큼..)
+			//java List 신축성있는 배열..
+			for(int i=0;i<list.size();i++){
+			Board board=(Board)list.get(i);
+		%>
+		<tr>
+			<td>Jill</td>
+			<td><%=board.getTitle() %></td>
+			<td><%=board.getWriter()%></td>
+			<td><%=board.getRegdate() %></td>
+			<td><%=board.getHit() %></td>
+		</tr>
+		<%} %>
+	</table>
+
+</body>
+</html>
+
+
+
+
